2ef0a1c80aad55c3efa53ceca8869c421890c5c8
northcutted <northcutted@gmail.com>
2026-05-02 14:23:37 -0500

    Initial Commit

---
+++ PicStrip/PicStripApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicStripApp.swift
+//  PicStrip
+//
+//  Created by Eddie Northcutt on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PicStripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
